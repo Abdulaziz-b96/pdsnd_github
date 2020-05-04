fd99697a50eea39823bea179a0cd7e528c57b262
Abdulaziz-b96 <64793756+Abdulaziz-b96@users.noreply.github.com>
2020-05-05 02:50:09 +0300

    First Comment Added

--- bikeshare.py
+++ bikeshare.py
@@ -75,7 +75,7 @@ def get_filters():
     print('-'*40)
     return city, month, day
 
-
+# get data for specidied city
 def load_data(city, month, day):
     """Load data for the specified filters of city(ies), month(s) and
        day(s) whenever applicable.
